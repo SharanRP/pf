b345ba3afec28f91a4deee204b316523062af2a5
SharanRP <z8903830@gmail.com>
2024-06-12 01:47:14 +0530

    Favicon updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,7 +13,7 @@ const About = () => {
           <p>
           Hi, I'm <b>Sharan Poojari</b>, a passionate coder from Mumbai, India, currently in my second year of BTech IT at VJTI. I code for the sheer joy of it and love exploring various technologies.
 As an enthusiastic learner, I'm constantly honing my skills and seeking out exciting coding challenges. In my free time, I enjoy working on personal projects that allow me to experiment and grow as a developer. You can check out some of my mini-projects and experiments in the projects section.
-I'm already making strides in the coding world. I'm thrilled to announce that I'm currently participating in the Google Summer of Code (GSOC) program with NumFOCUS, where I have the incredible opportunity to contribute to open-source projects. This experience has further fueled my passion for coding and my desire to make a meaningful impact in the developer community.
+I'm already making strides in the coding world. I'm thrilled to announce that I'm currently participating in the<b> Google Summer of Code (GSOC) </b>program with NumFOCUS, where I have the incredible opportunity to contribute to open-source projects. This experience has further fueled my passion for coding and my desire to make a meaningful impact in the developer community.
 I'm always eager to collaborate on new projects and initiatives where I can contribute my skills and continue to learn from others. If you're interested in working together or have any exciting opportunities, please don't hesitate to reach out. You can find my contact details in the footer.
 Let's connect and create something amazing together!
           </p>
--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -6,15 +6,15 @@ import {CgFileDocument} from "react-icons/cg";
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
     UniHubDesc:"Unishare is a platform designed to enrich the student experience by fostering connectivity, communication, and collaboration.Unishare serves as a multifaceted hub that goes beyond conventional social networks, offering a suite of features to enhance academic and social interactions.",
-    UniHubrGithub:"https://github.com/SharanRP/Unishare_Campus",
+    UniHubGithub:"https://github.com/SharanRP/Unishare_Campus",
     UniHubWebsite:"https://github.com/SharanRP/Unishare_Campus",
 
     VisualFlowDesc : "VisualFlow is a versatile platform that simplifies the process of creating flowcharts by offering an intuitive and user-friendly interface and generate code for the created flowchart",
     VisualFlowGithub : "https://github.com/SharanRP/VisualFlow",
-    VisualFlowWebsite : "https://github.com/SharanRP/VisualFlow/",
+    VisualFlowWebsite : "https://github.com/SharanRP/VisualFlow",
 
     Legal_AssistantDesc:"Our AI assistant can perform high-level searches to help users find the most relevant information ,also it churns out advice bespoke to the legal profession. Our AI-based legal assistant analyzes case law, gives sharp summaries with valuable insight, and provides tactical guidance for stronger legal arguments.",
-    Legal_AssistantlGithub:"https://github.com/SharanRP/legal_assistant",
+    Legal_AssistantGithub:"https://github.com/SharanRP/legal_assistant",
     Legal_AssistantWebsite:"https://legal-assistant-sigma.vercel.app",
   }
 
